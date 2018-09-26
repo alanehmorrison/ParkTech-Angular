b4c9146edec12872c03237602511e4c836578cb8
Ransford Walker <ransfordw@gmail.com>
2018-09-26 14:31:09 -0400

    deployment code stuff

--- src/app/Services/mypark.service.ts
+++ src/app/Services/mypark.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MyPark } from '../Shared/Models/MyPark';
-
-const ApiUrl = 'https://parksandtech.azurewebsites.net/api'
+import { ApiUrl } from '../../environments/environment.prod';
 
 @Injectable()
 export class MyParkService {
@@ -10,23 +9,23 @@ export class MyParkService {
   constructor(private _http: HttpClient) { }
 
   getAllMyParks(){
-    return this._http.get(`${ApiUrl}/mypark`, {headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}/api/mypark`, {headers: this.getHeaders() });
   }
 
   getMyParkByID(id: string){
-    return this._http.get(`${ApiUrl}/mypark/${id}`, { headers: this.getHeaders()});
+    return this._http.get(`${ApiUrl}/api/mypark/${id}`, { headers: this.getHeaders()});
   }
 
   createMyPark(myPark: MyPark){
-    return this._http.post(`${ApiUrl}/mypark`, myPark, {headers: this.getHeaders()});
+    return this._http.post(`${ApiUrl}/api/mypark`, myPark, {headers: this.getHeaders()});
   }
 
   deleteMyPark(id: number){
-    return this._http.delete(`${ApiUrl}/mypark/${id}`, { headers: this.getHeaders()});
+    return this._http.delete(`${ApiUrl}/api/mypark/${id}`, { headers: this.getHeaders()});
   }
 
   updateMyPark(myPark: MyPark){
-    return this._http.put(`${ApiUrl}/mypark`, myPark, { headers: this.getHeaders()});
+    return this._http.put(`${ApiUrl}/api/mypark`, myPark, { headers: this.getHeaders()});
   }
   private getHeaders(){
     return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
