cfb78aba1d01fede66b1e9917cb47c6ccb054cc5
Alaneh Morrison <alanehmorrison96@gmail.com>
2018-09-27 11:48:25 -0400

    login clear screen if invalid

--- src/app/Shared/login/login.component.ts
+++ src/app/Shared/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AbstractControl } from '@angular/forms/src/model';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -20,11 +22,21 @@ export class LoginComponent implements OnInit {
 
   createForm(){
     this.loginForm = this._form.group({
-      email: new FormControl,
-      password: new FormControl
+      email: new FormControl ('', Validators.required),
+      password: new FormControl ('', Validators.required)
     });
   }
 
   onSubmit(){
-    this._authService.login(this.loginForm.value)}
+    this._authService.login(this.loginForm.value)
+    let control: AbstractControl = null;
+    this.loginForm.reset();
+    this.loginForm.markAsUntouched();
+    Object.keys(this.loginForm.controls).forEach((name) =>{
+      control = this.loginForm.controls[name];
+      control.setErrors(null);
+    })
   }
+
+  
+}
