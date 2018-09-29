2f29097bf14ef7a9ef87d33324085085db348120
Ransford Walker <ransfordw@gmail.com>
2018-09-29 16:18:49 -0400

    enums displaying on trail delete view

--- src/app/components/trails/trail-delete/trail-delete.component.ts
+++ src/app/components/trails/trail-delete/trail-delete.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Trail } from '../../../Shared/Models/Trail';
+import { Component, OnInit, SimpleChange } from '@angular/core';
+import { Trail, Difficulty, Condition } from '../../../Shared/Models/Trail';
 import { TrailService } from '../../../Services/trail.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-trail-delete',
@@ -12,12 +13,16 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class TrailDeleteComponent implements OnInit {
 
   trail: Trail;
-  constructor(private _trailService: TrailService, private _ar: ActivatedRoute, private _router: Router) { 
+  difficultyDisplay: string;
+  conditionDisplay: string;
+  dataSource: MatTableDataSource<Trail>;
+
+  constructor(private _trailService: TrailService, private _ar: ActivatedRoute, private _router: Router) {
     this._ar.paramMap.subscribe(p => {
       this._trailService.getTrailByID(p.get('id')).subscribe((singleTrail: Trail) => {
         this.trail = singleTrail;
       });
-    })
+    });
   }
 
   onDelete() {
@@ -27,5 +32,16 @@ export class TrailDeleteComponent implements OnInit {
   }
 
   ngOnInit() {
+    this._ar.paramMap.subscribe(p => {
+      this._trailService.getTrailByID(p.get('id')).subscribe((singleTrail: Trail) => {
+        this.trail = singleTrail;
+        console.log(this.trail);
+        this.difficultyDisplay = Difficulty[singleTrail.TrailDifficulty];
+        this.trail.TrailDiff = this.difficultyDisplay;
+        console.log(this.difficultyDisplay);
+        this.conditionDisplay = Condition[singleTrail.TrailCondition];
+        this.trail.TempCondition = this.conditionDisplay;
+      });
+    });
   }
 }
